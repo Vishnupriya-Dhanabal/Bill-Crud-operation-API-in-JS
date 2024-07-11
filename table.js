5d882f9ce9d5d335c955377a3c0700b87c723b7b
vishnpriya-dhanabal <vishnudevi03@gmail.com>
2024-07-11 17:27:12 +0530

    screen finished

---
+++ table.js
@@ -0,0 +1,71 @@
+// document.addEventListener('DOMContentLoaded', function() {
+//     const formData = JSON.parse(localStorage.getItem('formData'));
+//     const tabledata = document.getElementById('tabledata');
+
+//     const row = document.createElement('tr');
+//     row.innerHTML = `
+//         <td>${formData.customerName}</td>
+//         <td>${formData.address}</td>
+//         <td>${formData.email}</td>
+//         <td>${formData.phoneNumber}</td>
+//         <td>${formData.gender}</td>
+//         <td>${formData.purchaseDate}</td>
+//         <td><button class="btn btn-primary" onclick="showInvoice()">Invoice</button></td>
+//     `;
+//     tabledata.appendChild(row);
+//     console.log(row)
+// });
+
+// function showInvoice() {
+//     window.location.href = 'invoice.html';
+// }
+
+// document.addEventListener('DOMContentLoaded', function() {
+//     const formData = JSON.parse(localStorage.getItem('formDat'));
+//     if (formData) {
+//         const tabledata = document.getElementById('tabledata');
+
+//         const row = document.createElement('tr');
+//         row.innerHTML = `
+//             <td>${formData.customerName}</td>
+//             <td>${formData.address}</td>
+//             <td>${formData.email}</td>
+//             <td>${formData.phoneNumber}</td>
+//             <td>${formData.gender}</td>
+//             <td>${formData.purchaseDate}</td>
+//             <td><button class="btn btn-primary" onclick="showInvoice()">Invoice</button></td>
+//         `;
+//         tabledata.appendChild(row);
+//     }
+// });
+
+// function showInvoice() {
+//     window.location.href = 'invoice.html';
+// }
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    const formDataArray = JSON.parse(localStorage.getItem('formDataArray'));
+    if (formDataArray) {
+        const tabledata = document.getElementById('tabledata');
+
+        formDataArray.forEach(formData => {
+            const row = document.createElement('tr');
+            row.innerHTML = `
+                <td>${formData.customerName}</td>
+                <td>${formData.address}</td>
+                <td>${formData.email}</td>
+                <td>${formData.phoneNumber}</td>
+                <td>${formData.gender}</td>
+                <td>${formData.purchaseDate}</td>
+                <td><button class="btn btn-primary" onclick="showInvoice()">Invoice</button></td>
+            `;
+            tabledata.appendChild(row);
+        });
+    }
+});
+
+function showInvoice() {
+    window.location.href = 'invoice.html';
+}
+
